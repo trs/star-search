607e9da9aa03999c0f333c9a2d75a3fec1f1663c
Tyler Stewart <hello@tylerstewart.ca>
2020-10-03 21:46:06 -0600

    mem cache

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { Provider as AuthProvider } from 'next-auth/client';
 import { GraphQLClient, ClientContext } from 'graphql-hooks';
+import memCache from 'graphql-hooks-memcache';
 
 import '../styles/tailwind.css';
 
 const client = new GraphQLClient({
-  url: 'https://api.github.com/graphql'
+  url: 'https://api.github.com/graphql',
+  cache: memCache()
 })
 
 import '../styles/globals.css';
